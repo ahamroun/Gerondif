801dd3483589ee99a39d0398067f8a96a5581e2c
JuCN <julien.charles-nicolas@insa-lyon.fr>
2017-04-11 15:58:33 +0200

    Utilisateur

--- Gerond'IF/Gerond'IF/Utilisateur.cpp
+++ Gerond'IF/Gerond'IF/Utilisateur.cpp
@@ -4,8 +4,58 @@
 
 Utilisateur::Utilisateur()
 {
+
 }
 
+
+Utilisateur::Utilisateur(string nom, string prenom, string email, string mdp)
+{
+	this->nom = nom;
+	this->prenom = prenom;
+	this->mail = email;
+	this->mdp = mdp;
+	this->statut = 0; 
+}
+
+string Utilisateur::getNom()
+{
+	return nom;
+}
+
+string Utilisateur::getPrenom()
+{
+	return prenom;
+}
+
+string Utilisateur::getMail()
+{
+	return mail;
+}
+
+string Utilisateur::getMdp()
+{
+	return mdp;
+}
+
+void Utilisateur::setNom(string n)
+{
+	nom = n;
+}
+
+void Utilisateur::setPrenom(string p)
+{
+	prenom = p;
+}
+
+void Utilisateur::setMail(string m)
+{
+	mail = m;
+}
+
+void Utilisateur::setMdp(string mdp)
+{
+	this->mdp = mdp;
+}
 
 Utilisateur::~Utilisateur()
 {
