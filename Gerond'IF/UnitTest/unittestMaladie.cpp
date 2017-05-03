f66f90daa4f247eb72ec1241fd595cb1dae5dcf8
Laurent <Laurent@LEOKIRI>
2017-05-03 11:19:23 +0200

    new file:   Gerond'IF/UnitTest/unittestMaladie.cpp 	new file:   Gerond'IF/UnitTest/unittestPatient.cpp

---
+++ Gerond'IF/UnitTest/unittestMaladie.cpp
@@ -0,0 +1,15 @@
+#include "CppUnitTest.h"
+#include "Maladie.h"
+#include <fstream>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest {
+	TEST_CLASS(UnitTestMaladie) {
+public:
+
+	TEST_METHOD(TestGetNom) {
+	}
+
+	};
+}
---
+++ Gerond'IF/UnitTest/unittestPatient.cpp
@@ -0,0 +1,50 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "Patient.h"
+#include <fstream>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{		
+	TEST_CLASS(UnitTestPatient)
+	{
+	public:
+		
+		TEST_METHOD(TestGetNom)
+		{
+			Patient p("Turpin", "Laurent", "lt@insa.fr");
+			Assert::AreEqual(
+				"Turpin",
+				p.getNom().c_str(),
+				L"failed",
+				LINE_INFO()
+			);
+		}
+		TEST_METHOD(TestChargerGenome1) {
+			Patient p("Turpin", "Laurent", "lt@insa.fr");
+			ifstream file("../Gerond'IF/Test_Genome.txt");
+			if(file) {
+				p.chargerGenome(file);
+				set<string> genome = p.getGenome();
+				Assert::IsFalse(genome.empty(), L"Genome Vide", LINE_INFO());
+			}
+			else {
+				Assert::Fail(L"Erreur lors de l'ouverture du fichier", LINE_INFO());
+			}
+		}
+		TEST_METHOD(TestChargerGenome2) {
+			Patient p("Turpin", "Laurent", "lt@insa.fr");
+			ifstream file("../Gerond'IF/Test_Genome.txt");
+			if(file) {
+				p.chargerGenome(file);
+				set<string> genome = p.getGenome();
+				string test = *(genome.begin());
+				Assert::AreEqual("AAA", test.c_str(), L"Mauvaise Lecture", LINE_INFO());
+			} else {
+				Assert::Fail(L"Erreur lors de l'ouverture du fichier", LINE_INFO());
+			}
+		}
+
+	};
+}
